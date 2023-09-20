83759c15262ecb0f30f0d05f945e0b0345e52108
Riad Zejnilagic Trumic <riiiaddesign@gmail.com>
2023-09-20 09:43:56 +0200

    [TASK] Prepare Release

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,8 @@
 <?php
 
-declare(strict_types=1);
-
 $EM_CONF[$_EXTKEY] = [
     'title' => '(FGTCLB) University Educational Course',
     'description' => 'Educational Course page for TYPO3 with structured data based on sys_category',
-    'category' => 'fe',
-    'state' => 'beta',
-    'version' => '0.1.0',
     'constraints' => [
         'depends' => [
             'typo3' => '11.5.0-11.5.99',
@@ -18,4 +13,9 @@ $EM_CONF[$_EXTKEY] = [
             'page_backend_layout' => '1.0-1.99',
         ],
     ],
+    'state' => 'beta',
+    'version' => '0.1.0',
+    'category' => 'fe',
+    'author_company' => 'FGTCLB GmbH',
+    'author_email' => 'hello@fgtclb.com',
 ];
